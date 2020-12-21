0aa9b095d4be044c5d8b5e55f8a3f5e4d925e08e
notEternal <n0tEternal@ya.ru>
2020-12-21 17:32:50 +0800

    Added usefull comment + deleted not worklist links

--- src/constants/abis/uniswap-v2-factory.ts
+++ src/constants/abis/uniswap-v2-factory.ts
@@ -1,5 +1,11 @@
 import UNISWAP_V2_FACTORY_ABI from './uniswap-v2-factory.json'
 
+// address for protocol fee: 0x10aA8C82e3656170baAE80D189b8b7DcEA6865C9
+// uniswap (all networks) 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
+
+// my rinkeby factory (for dev, with my address in the feeToSetter field )
+// 0xd9E20DDFBA01dcD86C7a4E82CCD3E6B8D3FA8221
+
 const UNISWAP_V2_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
 export { UNISWAP_V2_FACTORY_ADDRESS, UNISWAP_V2_FACTORY_ABI }
--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,21 +1,9 @@
 export const DEFAULT_TOKEN_LIST_URL =
   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
 
-// Smallest lists commented out
-
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'tokens.1inch.eth', // 1inch
-  'tokens.uniswap.eth', // uniswap
-  // 't2crtokens.eth', // kleros
-  // 'synths.snx.eth',
-  // 'tokenlist.dharma.eth',
-  // 'defi.cmc.eth',
-  // 'erc20.cmc.eth',
-  // 'stablecoin.cmc.eth',
-  // 'tokenlist.zerion.eth',
-  // 'tokenlist.aave.eth',
-  // 'https://umaproject.org/uma.tokenlist.json',
+  'https://umaproject.org/uma.tokenlist.json',
   'https://app.tryroll.com/tokens.json',
   'https://tokens.coingecko.com/uniswap/all.json',
   'https://defiprime.com/defiprime.tokenlist.json'
