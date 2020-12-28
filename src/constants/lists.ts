ee42e9c5a87da55c2953731a98134fc2a922b207
notEternal <n0tEternal@ya.ru>
2020-12-28 21:53:50 +0800

    Added token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -5,6 +5,6 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
   'https://umaproject.org/uma.tokenlist.json',
   'https://app.tryroll.com/tokens.json',
-  'https://tokens.coingecko.com/uniswap/all.json',
-  'https://defiprime.com/defiprime.tokenlist.json'
+  'https://tokens.coingecko.com/uniswap/all.json'
+  // 'https://clickswap.cloud/token-list/tokenlist.json'
 ]
